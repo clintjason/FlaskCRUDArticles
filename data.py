e18eb2cddf48327d6fe7f469ba1c675501419049
clintjason <ngassajason@yahoo.fr>
2018-01-28 23:52:15 +0100

    data files

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'author' : 'Brad Traversy',
+			'Create_date' : '04-25-27'
+		},
+		{
+			'id' : 2,
+			'title' : 'Article Two',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'author' : 'John Doe',
+			'Create_date' : '04-25-27'
+		},
+		{
+			'id' : 3,
+			'title' : 'Article THree',
+			'body' : 'Lorem	Ipsum dolor sit amet',
+			'author' : 'Brad Traversy',
+			'Create_date' : '04-25-27'
+		}
+	]
+	return articles
